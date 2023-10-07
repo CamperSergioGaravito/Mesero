8bd63029135039f41c803946cafc0c81cbba922e
CamperSergioGaravito <137243895+CamperSergioGaravito@users.noreply.github.com>
2023-10-07 06:53:58 -0500

    v0.0.3

--- js/eventoNuevoPedido.js
+++ js/eventoNuevoPedido.js
@@ -1,4 +1,4 @@
-import { templatePedidoRealizado } from "./templates.js";
+import { platosPedidoRealizado, templatePedidoRealizado } from "./templates.js";
 
 export function nuevoPedido(data) {
     const bdPedidos = JSON.parse(localStorage.getItem('pedidos'));
@@ -9,6 +9,13 @@ export function nuevoPedido(data) {
         const contPedidosCocina = document.getElementById('contPedidosCocina');
         const template = templatePedidoRealizado(data);
         contPedidosCocina.insertAdjacentHTML('beforeend',template);
+
+        const card = document.getElementById('card');
+        const comm = data.comanda;
+        console.log(comm);
+        comm.forEach(plat => {
+            card.insertAdjacentHTML('beforeend',platosPedidoRealizado(plat))
+        })
     }
     else {
         return
--- js/templates.js
+++ js/templates.js
@@ -106,38 +106,27 @@ export function templatePedidoRealizado(data) {
                         </div>
                         <div class="card-body bg-success" id="card">
                             <div class="card-title fs-2 text-white fw-bold">Mesa ${data.mesa}</div>
-                            <div class="input-group mb-3">
-                                <label aria-label="text input with checkbox" class="form-control">Plato 1</label>
-                                <div class="input-group-text bg-info">
-                                    <input type="checkbox" class="form-check-input mt-0" name="" id="" value="" aria-label="checkboxfor following text input" >
-                                </div>
+                            
+                            
                             </div>
-                            <div class="input-group mb-3">
-                                <label aria-label="text input with checkbox" class="form-control">Plato 2</label>
-                                <div class="input-group-text bg-info">
-                                    <input type="checkbox" class="form-check-input mt-0" name="" id="" value="" aria-label="checkboxfor following text input" >
-                                </div>
-                            </div>
-                            <div class="input-group mb-3">
-                                <label aria-label="text input with checkbox" class="form-control">Plato 3</label>
-                                <div class="input-group-text bg-info">
-                                    <input type="checkbox" class="form-check-input mt-0" name="" id="" value="" aria-label="checkboxfor following text input" >
-                                </div>
-                            </div>
-                            <div class="input-group mb-3">
-                                <label aria-label="text input with checkbox" class="form-control">Plato 4</label>
-                                <div class="input-group-text bg-info">
-                                    <input type="checkbox" class="form-check-input mt-0" name="" id="" value="" aria-label="checkboxfor following text input" >
-                                </div>
-                            </div>
-                            <div class="row gap-2 justify-content-center">
-                                <button class="btn btn-danger border border-white col-5" id="cancelarPedido">Cancelar</button>
-                                <button class="btn btn-primary border border-white col-5" id="terminarPedido">Terminar</button>
-                            </div>
-                        </div>
                         <div class="card-footer fs-5">Tiempo transcurrido 05:15:25 / 00:25:00</div>
                     </div>
                     `
     
     return template
+}
+
+export function platosPedidoRealizado(data) {
+    const tempItem = data.comanda;
+    const template = //html
+                    `
+                    <div class="input-group mb-3">
+                        <label aria-label="text input with checkbox" class="form-control">${data.nombre}</label>
+                        <div class="input-group-text bg-info">
+                            <input type="checkbox" class="form-check-input mt-0" name="" id="" value="" aria-label="checkboxfor following text input" >
+                        </div>
+                    </div>
+                    `
+
+    return 
 }
